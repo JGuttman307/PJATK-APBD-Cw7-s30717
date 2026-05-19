b48db35b683d6c9fe45a2d6ddd1b10097696607e
Justyna Guttman <justyna.guttman@gmail.com>
2026-05-19 22:34:39 +0200

    Changes

--- APBD-Cw7/APBD-Cw7/Models/PCComponents.cs
+++ APBD-Cw7/APBD-Cw7/Models/PCComponents.cs
@@ -1,6 +1,12 @@
 ﻿namespace APBD_Cw7.Models;
 
-public class PCComponents
+public class PCComponents //ważne
 {
-    
+    public int PCId { get; set; }
+    public PCs PC { get; set; }
+
+    public string ComponentCode { get; set; }
+    public Components Component { get; set; }
+
+    public int Amount { get; set; }
 }
